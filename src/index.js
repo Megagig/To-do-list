2fa4939f08b64dff5356fa30e5edc29043fb9874
ANTHONY OBI <Megagigsoftwaresolutions@gmail.com>
2023-05-09 11:26:39 +0100

    Add styles to the project

--- src/index.js
+++ src/index.js
@@ -1,4 +1,13 @@
 import './style.css';
-import sayHi from './modules/hi';
-import salut from './modules/salut';
-import welcom from './modules/welcom';
+
+const todoList = document.getElementById('todo-list');
+const todoInput = document.getElementById('todo-input');
+const form = document.querySelector('form');
+
+//Task Array
+const tasks = [
+  { description: 'Morning Prayer', completed: false, index: 0 },
+  { description: 'Morning Session meeting', completed: true, index: 1 },
+  { description: 'Lunch Break', completed: false, index: 2 },
+  { description: 'Finish the project for the day', completed: false, index: 3 },
+];
