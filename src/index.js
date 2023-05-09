0ecd22c03a6eaede2319820d251eaaf81b8b5957
ANTHONY OBI <Megagigsoftwaresolutions@gmail.com>
2023-05-09 19:51:25 +0100

    Remove all hardcoded items from the tasks array

--- src/index.js
+++ src/index.js
@@ -1,30 +1,80 @@
 import './style.css';
 import addNewTodo from './modules/Addnew.js';
-import createTodoListItemEl from './modules/createTodoListItem.js';
+// import createTodoListItemEl from './modules/createTodoListItem.js';
 
 const todoList = document.getElementById('todo-list');
 const todoInput = document.getElementById('todo-Input');
 const form = document.querySelector('form');
 
-// Task Array
-const tasks = [
-  { description: 'Morning Prayer', completed: false, index: 0 },
-  { description: 'Morning Session meeting', completed: true, index: 1 },
-  { description: 'Lunch Break', completed: false, index: 2 },
-  { description: 'Finish the project for the day', completed: false, index: 3 },
-];
+// Get the tasks from local storage
+let tasks = JSON.parse(localStorage.getItem('tasks')) || [];
+let editTodoDescription;
+let deleteTodo;
 
-// Add an event listener to the form element to handle todo list creation
-form.addEventListener('submit', (event) => {
-  event.preventDefault();
-  const todoDescription = todoInput.value;
-  if (todoDescription.trim() === '') {
-    return;
-  }
-  // Add the new task and reset the input field
-  addNewTodo(todoDescription, tasks, createTodoListItemEl, todoList);
-  todoInput.value = '';
-});
+const saveTodos = () => {
+  localStorage.setItem('tasks', JSON.stringify(tasks));
+};
+
+const createTodoLists = (task) => {
+  const deleteButton = document.createElement('button');
+  const listItemEl = document.createElement('li');
+  const iconEl = document.createElement('i');
+  const descriptionEl = document.createElement('span');
+
+  const checkboxEl = document.createElement('input');
+  checkboxEl.type = 'checkbox';
+  checkboxEl.checked = task.completed;
+
+  checkboxEl.addEventListener('change', () => {
+    task.completed = checkboxEl.checked;
+    saveTodos();
+
+    //check if the checkbox is checked
+    if (checkboxEl.checked) {
+      deleteButton.style.display = 'block';
+      iconEl.style.display = 'none';
+      listItemEl.style.display = 'flex';
+      listItemEl.style.justifyContent = 'flex-start';
+      deleteButton.style.marginLeft = 'auto';
+    } else {
+      deleteButton.style.display = 'none';
+      iconEl.style.display = 'block';
+      descriptionEl.style.color = '#999';
+    }
+  });
+
+  descriptionEl.textContent = task.description;
+  descriptionEl.addEventListener('click', () => {
+    editTodoDescription(task);
+  });
+
+  listItemEl.appendChild(checkboxEl);
+  listItemEl.appendChild(descriptionEl);
+
+  iconEl.classList.add('uil', 'uil-ellipsis-v');
+  iconEl.addEventListener('click', () => {
+    editTodoDescription(task);
+  });
+
+  listItemEl.appendChild(iconEl);
+
+  deleteButton.innerHTML = '<i class="uil uil-trash"></i>';
+  deleteButton.classList.add('delete-button');
+  deleteButton.style.display = 'none';
+
+  deleteButton.addEventListener('click', () => {
+    deleteTodo(task.index);
+  });
+
+  listItemEl.appendChild(deleteButton);
+  return listItemEl;
+};
+
+const updateTodoIndexes = () => {
+  tasks.forEach((task, index) => {
+    task.index = index;
+  });
+};
 
 const renderTodoList = () => {
   todoList.innerHTML = '';
@@ -35,6 +85,44 @@ const renderTodoList = () => {
       todoList.appendChild(listItemEl);
     });
 };
+
+deleteTodo = (index) => {
+  tasks = tasks.filter((task) => task.index !== index);
+  updateTodoIndexes();
+  saveTodos();
+  renderTodoList();
+};
+
+editTodoDescription = (task) => {
+  const inputEl = document.createElement('input');
+  inputEl.type = 'text';
+  inputEl.value = task.description;
+  inputEl.classList.add('edit-input');
+
+  inputEl.addEventListener('keydown', (event) => {
+    if (event.key === 'Enter') {
+      task.description = inputEl.value.trim();
+      saveTodos();
+      renderTodoList();
+    } else if (event.key === 'Escape') {
+      renderTodoList();
+    }
+  });
+
+  const listItemEl = todoList.children[task.index];
+  listItemEl.replaceChild(inputEl, listItemEl.children[1]);
+  inputEl.select();
+};
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+  const todoDescription = todoInput.value;
+  if (todoDescription.trim() === '') {
+    return;
+  }
+  addNewTodo(todoDescription);
+  todoInput.value = '';
+});
 
 renderTodoList();
 window.addEventListener('load', renderTodoList);
--- src/modules/Addnew.js
+++ src/modules/Addnew.js
@@ -1,9 +1,20 @@
-const addNewTodo = (description, tasks, createTodoListItemEl, todoList) => {
+// const addNewTodo = (description, tasks, createTodoListItemEl, todoList) => {
+//   const taskIndex = tasks.length;
+//   const task = { description, completed: false, index: taskIndex };
+//   tasks.push(task);
+//   saveTodos();
+//   const listItemEl = createTodoListItemEl(task);
+//   todoList.appendChild(listItemEl);
+// };
+
+function addNewTask(description) {
   const taskIndex = tasks.length;
+
   const task = { description, completed: false, index: taskIndex };
   tasks.push(task);
-  const listItemEl = createTodoListItemEl(task);
-  todoList.appendChild(listItemEl);
-};
+  saveTasks();
 
+  const listItemElement = createTaskLists(task);
+  taskList.appendChild(listItemElement);
+}
 export default addNewTodo;
