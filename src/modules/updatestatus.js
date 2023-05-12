fa930d0cf37124da25a90870e5c22e0def068c8a
ANTHONY OBI <Megagigsoftwaresolutions@gmail.com>
2023-05-12 16:53:30 +0100

    Remove commented code

--- src/modules/updatestatus.js
+++ src/modules/updatestatus.js
@@ -7,17 +7,3 @@ const incompleteTo = (task) => {
 };
 
 export { completedTo, incompleteTo };
-
-// export const completedTo = (task) => {
-//   return {
-//     ...task,
-//     completed: true,
-//   };
-// };
-
-// export const incompleteTo = (task) => {
-//   return {
-//     ...task,
-//     completed: false,
-//   };
-// };
